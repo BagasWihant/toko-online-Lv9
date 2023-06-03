dfb3a082e5afc0c0c863ee53425ac2568f1490be
Wihant <wihantb@gmail.com>
2023-06-03 18:20:29 +0700

    add soft ui dashboard ke admin dashboard

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Auth;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,5 +15,14 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('layouts.admin');
+});
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+// admin
+Route::prefix('/admin')->group( function(){
+    Route::get('dashboard', [App\Http\Controllers\Admin\DashboardController::class, 'index']);
 });
